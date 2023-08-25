d1b7eebfb8813d38f20500d70839bae9455ff92e
ednalima-gb <edna.lima@grupoboticario.com.br>
2023-08-25 14:24:16 -0300

    feat:finalizando questoes

--- build/rascunho.js
+++ build/rascunho.js
@@ -1,1 +1,21 @@
 "use strict";
+class Produtos {
+    constructor(codigo, nome, preco, quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+}
+class Estoque {
+    constructor() {
+        this.produtos = [];
+    }
+    adicionarProduto(produto) {
+        this.produtos.push(produto);
+        console.log(produto);
+    }
+}
+const estoque = new Estoque();
+const produto1 = new Produtos(1, "Camiseta", 25, 10);
+estoque.adicionarProduto(produto1);
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,44 @@
 //questão 1
+console.log('-----------> Questão 1 <-----------')
 
-// class Alunos {
-//     matricula:number
-//     nome:string
-//     nota_prova1:number
-//     nota_prova2:number
-//     nota_trabalho: number
+class Alunos {
+    matricula:number
+    nome:string
+    nota_prova1:number
+    nota_prova2:number
+    nota_trabalho: number
 
-//     constructor(matricula:number, nome:string, nota_prova1:number, nota_prova2:number, nota_trabalho: number){
-//         this.matricula = matricula
-//         this.nome = nome
-//         this.nota_prova1 = nota_prova1
-//         this.nota_prova2 = nota_prova2
-//         this.nota_trabalho = nota_trabalho
-//     }
+    constructor(matricula:number, nome:string, nota_prova1:number, nota_prova2:number, nota_trabalho: number){
+        this.matricula = matricula
+        this.nome = nome
+        this.nota_prova1 = nota_prova1
+        this.nota_prova2 = nota_prova2
+        this.nota_trabalho = nota_trabalho
+    }
 
-//     media(){
-//         const media = (this.nota_prova1 * 2.5 + this.nota_prova2 * 2.5 + this.nota_trabalho * 2)/ 7
-//         console.log(`${this.nome} sua média é ${media}`)
-//     }
+    media(){
+        const media = (this.nota_prova1 * 2.5 + this.nota_prova2 * 2.5 + this.nota_trabalho * 2)/ 7
+        console.log(`${this.nome} sua média é ${media}`)
+    }
 
-//     final(){
-//         const nota_para_passar = 7
-//         const media = (this.nota_prova1 * 2.5 + this.nota_prova2 * 2.5 + this.nota_trabalho * 2)/ 7
-//         if(media > 3 && media < nota_para_passar){
-//             const falta_para_passar = (nota_para_passar - media) + nota_para_passar
-//             console.log(`${this.nome} você precisa tirar ${falta_para_passar} para passar na final ` )
-//         }else if (media >= 7){
-//             console.log(0)
-//         }else{
-//             console.log('Reprovado(a)')
-//         }
-//     }
-// }  
+    final(){
+        const nota_para_passar = 7
+        const media = (this.nota_prova1 * 2.5 + this.nota_prova2 * 2.5 + this.nota_trabalho * 2)/ 7
+        if(media > 3 && media < nota_para_passar){
+            const falta_para_passar = (nota_para_passar - media) + nota_para_passar
+            console.log(`${this.nome} você precisa tirar ${falta_para_passar} para passar na final ` )
+        }else if (media >= 7){
+            console.log(0)
+        }else{
+            console.log('Reprovado(a)')
+        }
+    }
+}  
 
-// const aluno_matriculado = new Alunos (2420, 'Ana', 7, 7, 7)
+const aluno_matriculado = new Alunos (2420, 'Ana', 7, 7, 7)
 
-// aluno_matriculado.media()
-// aluno_matriculado.final()
+aluno_matriculado.media()
+aluno_matriculado.final()
 
 //questão 2
 console.log('-----------> Questão 2 <-----------')
@@ -256,19 +257,151 @@ vooPorAlas.tipo(85)
 
 //questão 5
 
-// console.log('-----------> Questão 5 <-----------')
+ console.log('-----------> Questão 5 <-----------')
 
-// class Produtos {
-//     nome:string
-//     preco:number
-//     codigo:number
+ var prompt_ = require('prompt-sync')();
 
-//     const produtos = ['camisa', 'casaco', 'saia']
-// }
+function exibirMenu() {
+    console.log("Este é o menu de opções disponível: ")
+    console.log("1. Adicionar produto");
+    console.log("2. Remover produto");
+    console.log("3. Alterar produto");
+    console.log("4. Buscar produto");
+    console.log("5. Listar produtos");
+    console.log("6. Calcular valor total do estoque");
+    console.log("0. Sair");
+  }
 
-// class Estoque {
-//     produtos:Array<Produtos>
+  function main() {
+    let choice = -1;
+    while (choice !== 0) { 
+      exibirMenu();
+      choice = parseInt(prompt_("Escolha uma opção:"));
+      switch (choice) {
+        case 1:
+          // Adicionar produto
+          const codigo = parseInt(prompt_("Digite o código do produto:"));
+          const nome = prompt_("Digite o nome do produto:");
+          const preco = parseFloat(prompt_("Digite o preço do produto:"));
+          const quantidade = parseInt(prompt_("Digite a quantidade do produto:"));
+          const novoProduto = new Produtos(codigo, nome, preco, quantidade);
+          estoque.adicionarProduto(novoProduto);
+          break;
+        case 2:
+          // Remover produto
+          const codigoRemover = parseInt(prompt_("Digite o código do produto a ser removido:"));
+          estoque.removerProduto(codigoRemover);
+          break;
+        case 3:
+          // Alterar produto
+          const codigoAlterar = parseInt(prompt_("Digite o código do produto a ser alterado:"));
+          const produtoExistente = estoque.buscarProduto(codigoAlterar);
+          if (produtoExistente) {
+            const novoNome = prompt_("Digite o novo nome do produto:");
+            const novoPreco = parseFloat(prompt_("Digite o novo preço do produto:"));
+            const novaQuantidade = parseInt(prompt_("Digite a nova quantidade do produto:"));
+            const novoProdutoAlterado = new Produtos(codigoAlterar, novoNome, novoPreco, novaQuantidade);
+            estoque.alterarProduto(codigoAlterar, novoProdutoAlterado);
+          } else {
+            console.log("Produto não encontrado.");
+          }
+          break;
+        case 4:
+          // Buscar produto
+          const codigoBuscar = parseInt(prompt_("Digite o código do produto a ser buscado:"));
+          const produtoBuscado = estoque.buscarProduto(codigoBuscar);
+          if (produtoBuscado) {
+            console.log("Produto encontrado:", produtoBuscado);
+          } else {
+            console.log("Produto não encontrado.");
+          }
+          break;
+        case 5:
+          // Listar produtos
+          const listaProdutos = estoque.listarProdutos();
+          console.log("Lista de produtos no estoque:");
+          listaProdutos.forEach(produto => {
+            console.log(produto);
+          });
+          break;
+        case 6:
+          // Calcular valor total do estoque
+          const valorTotal = estoque.calculoValorTotal();
+          console.log("Valor total do estoque:", valorTotal.toFixed(2));
+          break;
+        case 0:
+          // Sair
+          console.log("Encerrando o programa.");
+          break;
+        default:
+          console.log("Opção inválida. Digite um número válido.");
+      }
+    }
+  }
+
+class Produtos {
+    codigo:number
+    nome:string
+    preco:number
+    quantidade:number
+
+    //const produtos = ['camisa', 'casaco', 'saia']
+
+    constructor(codigo:number, nome:string, preco:number, quantidade:number){
+        this.codigo = codigo
+        this.nome = nome
+        this.preco = preco
+        this.quantidade = quantidade
+    }
+}
+
+class Estoque {
+    produtos:Array<Produtos>
+
+    constructor(){
+        this.produtos = [];
+    }
+
+    adicionarProduto(produto:Produtos) {
+        this.produtos.push(produto);
+    }
+
+    removerProduto(codigo:number){
+        const index = this.produtos.findIndex(produto => produto.codigo === codigo);
+        if (index !== -1) {
+            this.produtos.splice(index, 1);
+        }
+    }
+
+   alterarProduto(codigo:number, novoProduto:Produtos){
+        const index = this.produtos.findIndex(produto => produto.codigo === codigo);
+            if(index !== -1){
+                this.produtos[index] = novoProduto
+            }
+   }
+
+   buscarProduto(codigo:number){
+        return this.produtos.find(produto => produto.codigo === codigo)
+    }
+
+    listarProdutos(){
+        return this.produtos
+    }
+
+    calculoValorTotal(){
+        return this.produtos.reduce((total, produto) => total + produto.preco, 0)
+    }
+
+}
+
+const estoque = new Estoque()
+const produto1 = new Produtos(1, "Vestido", 100, 2)
+const produto2 = new Produtos(2, "saia", 60, 3)
+
+estoque.adicionarProduto(produto1)
+estoque.adicionarProduto(produto2)
+
+main()
 
 
 
-// }
